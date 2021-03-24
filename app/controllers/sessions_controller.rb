c5845ecadde173624b2ba889e4455d0bbdcf965d
Faheem <qazi.faheem@nxb.com.pk>
2021-03-24 20:13:09 +0500

    added routes for login or logout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,7 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by(params[:session][:username])
+    user = User.find_by(username: params[:session][:username])
     if( user && user.authenticate(params[:session][:password]))
       session[:user_id] = user.id
       flash[:success]= " Successfully logged in "
