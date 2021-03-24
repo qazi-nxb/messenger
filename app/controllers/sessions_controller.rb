369dce5485ab81ea4e43c35ee99c28c5dd032e48
Faheem <qazi.faheem@nxb.com.pk>
2021-03-24 20:04:07 +0500

    added restrictions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,25 @@
 class SessionsController < ApplicationController
 
   def new
+  end
+
+  def create
+    user = User.find_by(params[:session][:username])
+    if( user && user.authenticate(params[:session][:password]))
+      session[:user_id] = user.id
+      flash[:success]= " Successfully logged in "
+      redirect_to root_path
+    else
+      flash.now[:danger] = " You have enetred something  wrong "
+      render 'new'
+    end
 
   end
+
+  def destroy
+    session[:user_id]= nil
+    flash[:success] = "Logged Out Sccess fully"
+    redirect_to login_path
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   resources :chatbox
   root :to => "chatbox#index"
   get 'login', to: "sessions#new"
+  post 'login', to: "sessions#create"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
