0c5e6f3f9c557a45283d53f9e479a78706e83d48
Faheem <qazi.faheem@nxb.com.pk>
2021-03-25 11:49:53 +0500

    Added flash messages

--- app/controllers/chatbox_controller.rb
+++ app/controllers/chatbox_controller.rb
@@ -1,5 +1,10 @@
 class ChatboxController < ApplicationController
   def index
-    @msg = Message.all
+    if logged_in?
+      @msg = Message.all
+    else
+      flash[:danger] = "You should logged in first"
+      redirect_to login_path
+    end
   end
 end
