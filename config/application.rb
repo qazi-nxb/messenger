f4706dcf930724dfb78241a731669599344f5efc
Faheem <qazi.faheem@nxb.com.pk>
2021-03-29 16:23:09 +0500

    Gem file error resolved

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,8 @@ module Messenger
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
+    config.assets.initialize_on_precompile = false
+
     config.action_cable.mount_path = '/cable'
     # Configuration for the application, engines, and railties goes here.
     #
